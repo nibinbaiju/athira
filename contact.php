e9ff0d6ab6baa1273d074e5900e00c3575493eb6
nibinbaiju <nibinbaiju@gmail.com>
2021-03-17 02:13:02 +0530

    next

---
+++ contact.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="">RCSS</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+              </li>
+          <li class="nav-item">
+            <a class="nav-link active"a aria-current="page" href="aboutus.php">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active"a aria-current="page" href="gallery.php">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active"a aria-current="page" href="dept.php">Department</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active"a aria-current="page" href="contact.php">Contact Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+<br><br>
+
+
+
+
+<body>
+        
+  <!--<div class="show_loader">-->
+  <!--    <div class="loader_outer">-->
+  <!--        <div class="loader"></div>-->
+  <!--    </div>-->
+  <!--</div>-->
+  <div class="wrp-all">
+      <div class="modal" id="virtual_tour">
+<div class="modal-dialog">
+  <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+          <h4 class="modal-title">RCSS - Virtual Tour</h4>
+          <button type="button" class="close" data-dismiss="modal">×</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body" id="virtual-tour-content">
+          <div class="container">
+              <div class="row">
+                                                  <div class="col-lg-6">
+                              <a target="_blank" href="/virtual-tour/hill-campus/" class="vid_box" style="background: url(/uploads/v-tour/1/1.jpg);"></a>
+                              <div class="frame_title">Hill Campus</div>
+                          </div>
+                                                          <div class="col-lg-6">
+                              <a target="_blank" href="/virtual-tour/valley-campus/" class="vid_box" style="background: url(/uploads/v-tour/2/2.jpg);"></a>
+                              <div class="frame_title">Valley Campus</div>
+                          </div>
+                                              </div>
+          </div>
+      </div>
+
+  </div>
+</div>
+</div>
+  <br>
+<figcaption>
+  <div class="container">
+      <h1>Contact Us</h1>
+  </div>
+</figcaption>
+</section>
+
+
+<br><br>
+
+<section class="inner-content bgcontent contact-wrp">
+
+<div class="container">
+
+  <div class="info-box">
+      <div class="row">
+          <div class="col-md-4">
+              <form class="form-part contact-enquiry">
+                  <h2>Enquiry/Feedback</h2>
+                  <div class="row ">
+                      <div class="col-12 col-md-12 cpad">
+                          <div class="form-group">
+                              <input type="text" id="name" name="name" class="form-control" aria-describedby="emailHelp" placeholder="Enter your Name" required="">
+                          </div>
+                      </div>
+                      <div class="col-12 col-md-12 cpad">
+                          <div class="form-group">
+                              <input type="text" id="address" name="address" class="form-control" aria-describedby="emailHelp" placeholder="Enter your Address" required="">
+                          </div>
+                      </div>
+                      <div class="col-12 col-md-12 cpad">
+                          <div class="form-group">
+                              <input type="email" id="email" name="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter your Email Id" required="">
+                          </div>
+                      </div>
+                      <div class="col-12 col-md-12 cpad">
+                          <div class="form-group">
+                              <input type="text" id="phone" name="phone" class="form-control" aria-describedby="emailHelp" placeholder="Enter your Contact No." required="">
+                          </div>
+                      </div>
+                      <div class="col-12 cpad">
+                          <div class="form-group">
+                              <textarea class="form-control" name="message" id="message" rows="5" placeholder="Enquiry/Feedback" required=""></textarea>
+                          </div>
+                      </div>
+                  </div>
+                  <button class="submit-btn">Submit</button>
+              </form>
+          </div>
+          <div class="col-md-8">
+              <h2>Contact Info</h2>
+              <div class="row">
+                  <div class="col-md-6">
+                      <div class="contact-tab">
+                          <nav>
+                              <div class="nav  nav-pills" id="nav-tab" role="tablist">
+                                  <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="pill" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Hill Campus Kalamassery</a>
+                              </div>
+                          </nav>
+                          <div class="tab-content" id="nav-tabContent">
+                              <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+
+                                  <p>Rajagiri College of Social Sciences (Autonomous), <br>Rajagiri P.O, <br>Kalamassery, Cochin - 683 104, <br>Kerala, India.</p>
+
+                                  <ul class="list-unstyled contact-list">
+                                      <li><a href="#"><i class="fa fa-envelope-open"> </i> :  admin@rajagiri.edu (Office) <br> &nbsp; &nbsp; &nbsp; &nbsp; rcssadmission@rajagiri.edu (Admissions)</a></li>
+                                      <li><a href="#"><i class="fa fa-phone"> </i> :  +91 484 - 2911111 / 2911507</a></li>
+                                      <li><a href="#"><i class="fa fa-fax"> </i> :  +91 484 - 2532862</a></li>
+                                  </ul>
+
+
+
+
+                              </div>
+                              <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                  <p></p><p>Rajagiri College of Social Sciences (Autonomous), <br>Rajagiri Valley P. O, <br>Kakkanad, Kochi - 682 039, <br>Kerala, India.</p>
+
+                                  <ul class="list-unstyled contact-list">
+                                      <li><a href="#"><i class="fa fa-envelope-open"> </i> :  rsom@rajagiri.edu (Office) <br> &nbsp; &nbsp; &nbsp; &nbsp; admission@rajagiri.edu (Admissions)</a></li>
+                                      <li><a href="#"><i class="fa fa-phone"> </i> :  +91 - 484 - 2660555 / 2660609</a></li>
+                                      <li><a href="#"><i class="fa fa-fax"> </i> :  +91 484 - 2426578</a></li>
+                                  </ul>
+                              </div>
+                          </div>
+
+
+                      </div>
+                  </div>
+                  <div class="col-md-6">
+                      <div class="contact-tab">
+
+                          <nav>
+                              <div class="nav  nav-pills" id="nav-tab" role="tablist">
+                                  <a class="nav-item nav-link active" id="nav-profile-tab" data-toggle="pill" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Valley Campus Kakkanad</a>
+                              </div>
+                          </nav>
+                          <div class="tab-content" id="nav-tabContent">
+                              <div class="tab-pane fade show active" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                  <p></p><p>Rajagiri College of Social Sciences (Autonomous), <br>Rajagiri Valley P. O, <br>Kakkanad, Kochi - 682 039, <br>Kerala, India.</p>
+
+                                  <ul class="list-unstyled contact-list">
+                                      <li><a href="#"><i class="fa fa-envelope-open"> </i> :  rsom@rajagiri.edu (Office) <br> &nbsp; &nbsp; &nbsp; &nbsp; admission@rajagiri.edu (Admissions)</a></li>
+                                      <li><a href="#"><i class="fa fa-phone"> </i> :  +91 - 484 - 2660555 / 2660609</a></li>
+                                      <li><a href="#"><i class="fa fa-fax"> </i> :  +91 484 - 2426578</a></li>
+                                  </ul>
+                              </div>
+                          </div>
+
+
+                      </div>
+                  </div>
+              </div>
+
+
+          </div>
+      </div>
+  </div>
+  <br><br>
+                                              <li>
+                                                  <a href="dept.php">Dept. of Computer Science</a>  
+                                                <br><br>                                                  </li>
+                                                                                                  
+                      <div class="col-12 col-md-12 col-lg-6">
+                          <div class="row">
+                              <div class="col-12 col-sm-6 col-md-6 col-lg-6 foot-contact">
+                                  <h3>Hill Campus Kalamassery</h3>
+                                  <p>Rajagiri College of Social Sciences (Autonomous), Rajagiri P.O, Kalamassery, Cochin - 683 104, Kerala, India.</p>
+<br><br>                      
+                                  <ul>
+<li><strong>General Contacts</strong></li>
+<li>Email: admin@rajagiri.edu</li>
+<li>Phone: +91 484 - 2911111 / 2911507</li>
+<li>Fax: +91 484 - 2532862</li>
+<li>&nbsp;</li>
+<li><strong>Admission Contacts</strong></li>
+<li>Phone: +91 484 - 2660601</li>
+<li>Mobile: +91-9645081287</li>
+<li>Email: rcssadmission@rajagiri.edu&nbsp;</li>
+<li><span style="color:#999999"><em>For Programs:</em></span></li>
+<li>[ MSW, MCA, MSc Psychology, MLISC ]</li>
+<li>[ BCOM, BBA, BSW, BLISC, BSW,BSc Psychology]</li>
+</ul>
+                                                                      </div>
+                              <div class="col-12 col-sm-6 col-md-6 col-lg-6  foot-contact">
+                                  <h3>Valley Campus Kakkanad</h3>
+                                  <p>Rajagiri College of Social Sciences (Autonomous), Rajagiri Valley P. O, Kakkanad,Kochi - 682 039, Kerala, India.</p>
+                                                                              <ul>
+<li><strong>General Contacts:</strong></li>
+<li>Email: rsom@rajagiri.edu</li>
+<li>Phone: +91 - 484 - 2660555 / 2660609</li>
+<li>Fax: +91 - 484 - 2426578</li>
+<li>&nbsp;</li>
+<li><strong>Admission Contacts&nbsp;</strong></li>
+<li>Phone: +91 484 - 2660601</li>
+<li>Mobile: +91-9645081287</li>
+<li>Email: admission@rajagiri.edu</li>
+<li><span style="color:#999999"><em>For Programs:</em></span></li>
+<li>[ MBA, MHRM ]<br>
+&nbsp;</li>
+</ul>
+                                                                      </div>
+                          </div>
+                      </div>
+
+                  </div>
+              </div>
+          </div>
+
+   
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
